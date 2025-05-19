e4d2e17e6ce2ab6a01108d1830d5e84ae40110a3
CarolineBraga <carolinebraga.guimaraes@gmail.com>
2025-05-19 21:37:43 +0100

    Melhorias de layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.appmotivation"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.appmotivation"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -27,12 +27,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "21"
     }
+    buildFeatures{
+        viewBinding = true
+
+    }
+
 }
 
 dependencies {
--- app/src/main/java/com/example/appmotivation/FrasesActivity.kt
+++ app/src/main/java/com/example/appmotivation/FrasesActivity.kt
@@ -1,20 +1,34 @@
 package com.example.appmotivation
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.appmotivation.databinding.ActivityFrasesBinding
 
-class FrasesActivity : AppCompatActivity() {
+
+class FrasesActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityFrasesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityFrasesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.buttonNew.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        TODO("Not yet implemented")
     }
 }
