03da923cbc50e520b0ea1305c1e83c0489ac8b76
CarolineBraga <carolinebraga.guimaraes@gmail.com>
2025-06-17 21:09:05 +0100

    Implementação de melhorias de ui

--- app/src/main/java/com/example/appmotivation/ui/PhrasesActivity.kt
+++ app/src/main/java/com/example/appmotivation/ui/PhrasesActivity.kt
@@ -31,7 +31,7 @@ class PhrasesActivity : AppCompatActivity(), View.OnClickListener {
 
         securityPreferences = SecurityPreferences(this)
 
-      /*  ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+      /*   ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
